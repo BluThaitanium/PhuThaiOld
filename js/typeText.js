2ed69a1f6a4c5d3c8cdeb65f1f2f4ad19bf4d698
BluThaitanium <47192641+BluThaitanium@users.noreply.github.com>
2021-05-27 17:28:45 -0400

    Uploaded in./root so github pages works

---
+++ js/typeText.js
@@ -0,0 +1,35 @@
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+canvas.width = canvas.height * 
+    (canvas.clientWidth / canvas.clientHeight);
+
+let counter = 0;
+
+let xx = 0;
+let xxx = 0;
+let word = "console.log('Phu Bar'); ";
+
+setInterval(function() {
+    context.fillStyle = (counter % 2 === 0) ? 'lightgreen' : 'blue';
+    context.font = "bold 16px Arial";
+    context.fillRect(4*xx, 1, 20, 50);
+	  counter++;
+
+    if (counter%4 === 0) {
+    	xx += 10;
+      xxx += 10;
+      context.fillStyle = 'lightgreen';
+      context.font = "40px Arial";
+     	context.fillText(word[xxx/10 - 1], 4*xxx - 40, 45);
+
+    } else if (counter%98 === 0){
+      xx = 0;
+      xxx = 0;
+      counter = 0;
+    	context.clearRect(0, 0, canvas.width, canvas.height);
+    }
+
+
+
+}, 50);
